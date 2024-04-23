496add52ccdc84393ecfc64f698ebe8ddc48a3c0
zelouassdi <zakaria.el-ouassdi@hotmail.com>
2024-04-23 21:26:09 +0200

    Adding CrossOrigin Filter

--- webApp/src/main/java/com/zakprojects/webApp/EmployeeResource.java
+++ webApp/src/main/java/com/zakprojects/webApp/EmployeeResource.java
@@ -4,7 +4,6 @@ import com.zakprojects.webApp.Service.EmployeeService;
 import com.zakprojects.webApp.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
